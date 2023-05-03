c17e3c8e1ade0b44bb180f84ed78ae19335735a8
Masashi Fujita <masashifujita536@gmail.com>
2023-05-04 01:37:46 +0900

    remove vendor/bundle

--- vendor/bundle/ruby/3.1.0/gems/propshaft-0.7.0/lib/propshaft/helper.rb
+++
@@ -1,7 +0,0 @@
-module Propshaft
-  module Helper
-    def compute_asset_path(path, options = {})
-      Rails.application.assets.resolver.resolve(path) || raise(MissingAssetError.new(path))
-    end
-  end
-end

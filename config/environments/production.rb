b39da0ee7251ac9d656d56718dda97fe70de713b
Masashi Fujita <masashifujita536@gmail.com>
2023-06-11 21:17:08 +0900

    rename app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "lets_readable_code_production"
+  # config.active_job.queue_name_prefix = "readable_coder_production"
 
   config.action_mailer.perform_caching = false
 
